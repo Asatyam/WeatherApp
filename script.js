a7c930379fc57dffd5f771ef477346f486960ffb
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-04-14 22:46:54 +0530

    Add the function to display weather details in card

--- script.js
+++ script.js
@@ -39,25 +39,57 @@ function createSearchBtn() {
 function createCard() {
   const cardDiv = document.createElement('div');
   cardDiv.setAttribute('class', 'card');
+  const cityPara = document.createElement('p');
+  cityPara.className = 'city';
+  const tempPara = document.createElement('p');
+  tempPara.className = 'temp';
+  const feelsPara = document.createElement('p');
+  feelsPara.className = 'feels';
+  const humidityPara = document.createElement('p');
+  humidityPara.className = 'humidity';
+  const windPara = document.createElement('p');
+  windPara.className = 'wind';
+  cardDiv.appendChild(cityPara);
+  cardDiv.appendChild(tempPara);
+  cardDiv.appendChild(feelsPara);
+  cardDiv.appendChild(humidityPara);
+  cardDiv.appendChild(windPara);
   const content = document.querySelector('.content');
   content.appendChild(cardDiv);
 }
 async function getWeatherData(location) {
+  const loc = location.toLowerCase() || 'london';
   const forecast = await fetch(
     // eslint-disable-next-line comma-dangle
-    `https://api.weatherapi.com/v1/current.json?key=469b2f87163341f88fc112429231404&q=${location}`
+    `https://api.weatherapi.com/v1/current.json?key=469b2f87163341f88fc112429231404&q=${loc}`
   );
   const weatherData = await forecast.json();
   console.table(weatherData);
+  return weatherData;
 }
+async function displayCard(weather) {
+  const weatherData = weather || (await getWeatherData('London'));
+  const city = document.querySelector('.city');
+  const temp = document.querySelector('.temp');
+  const feels = document.querySelector('.feels');
+  const humidity = document.querySelector('.humidity');
+  const wind = document.querySelector('.wind');
+  city.textContent = weatherData.location.name.toUpperCase();
+  temp.textContent = weatherData.current.temp_c;
+  feels.textContent = weatherData.current.feelslike_c;
+  humidity.textContent = weatherData.current.humidity;
+  wind.textContent = weatherData.current.wind_kph;
+}
+
 async function searchLocation() {
   const form = document.querySelector('form');
   const search = document.querySelector('.search');
 
-  form.addEventListener('submit', (e) => {
+  form.addEventListener('submit', async (e) => {
     e.preventDefault();
     const location = search.value.toLowerCase();
-    getWeatherData(location);
+    const weatherData = await getWeatherData(location);
+    displayCard(weatherData);
   });
 }
 
@@ -67,5 +99,5 @@ createForm();
 createSearchBar();
 createSearchBtn();
 createCard();
-getWeatherData();
 searchLocation();
+displayCard();
